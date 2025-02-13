f4c7911cc37dd85bf597db8955704921b0315a75
magentix <mvion@magentix.fr>
2025-02-13 13:00:42 +0100

    Do not cache API errors

--- src/MagentoApiClient.php
+++ src/MagentoApiClient.php
@@ -102,7 +102,7 @@ class MagentoApiClient
             'result' => is_array($data) ? $data : $response,
         ];
 
-        if ($this->canCache($method) && isset($cacheKey)) {
+        if ($this->canCache($method, $result) && isset($cacheKey)) {
             $this->cache->set($cacheKey, $result);
         }
 
@@ -174,8 +174,8 @@ class MagentoApiClient
         return implode('&', $return);
     }
 
-    protected function canCache(string $method): bool
+    protected function canCache(string $method, array $result): bool
     {
-        return strtoupper($method) === 'GET' && $this->cache !== null;
+        return $this->cache !== null && strtoupper($method) === 'GET' && !($result['error'] ?? true);
     }
 }
